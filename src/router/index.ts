ffa4af503acc20540f7f495d29984a25efe799fc
ItaloCanturil <italo.cantur@gmail.com>
2025-07-27 17:33:03 -0300

    feat(router): add vue-router integration

---
+++ src/router/index.ts
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('../layouts/MainLayout.vue'),
+    children: [
+      {
+        path: '/',
+        component: () => import('../views/HomePage.vue'),
+      },
+      {
+        path: '/auth',
+        component: () => import('../features/authentication/views/AuthView.vue'),
+        redirect: '/auth/login',
+        children: [
+          {
+            path: 'login',
+            name: 'Login',
+            component: () => import('../features/authentication/components/LoginSide.vue'),
+          },
+          {
+            path: 'register',
+            name: 'Register',
+            component: () => import('../features/authentication/components/RegisterSide.vue'),
+          },
+        ],
+      }
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
