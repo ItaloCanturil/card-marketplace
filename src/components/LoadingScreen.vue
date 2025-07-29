23844230ae340e7948f12b673d2b603913c1fe71
ItaloCanturil <italo.cantur@gmail.com>
2025-07-28 22:44:25 -0300

    feat: create LoadingScreen component

---
+++ src/components/LoadingScreen.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
+</script>
+
+<template>
+  <div class="fixed inset-0 bg-black opacity-50 flex items-center justify-center">
+    <DotLottieVue style="height: 500px; width: 500px" autoplay loop
+      src="https://lottie.host/284b71f5-f450-4c7f-b0f2-c7c1dcd2e1dd/cIUqo24oLd.lottie" />
+  </div>
+</template>
