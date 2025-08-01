88026605d2f5162078ada06bfa02bcab1203978b
Italo Canturil <italo.cantur@gmail.com>
2025-08-01 09:18:52 -0300

    feat: add home background image

--- src/components/CardBackground.vue
+++ src/components/CardBackground.vue
@@ -3,7 +3,7 @@
 
 <template>
   <div class="absolute inset-0 z-0 overflow-hidden">
-    <div class="flex flex-wrap justify-center items-center gap-6 h-full opacity-50">
+    <div class="flex flex-wrap justify-center items-center gap-6 h-full opacity-50 bg-[url('/public/bg-home.png')]">
 
     </div>
   </div>
